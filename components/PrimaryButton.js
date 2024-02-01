88876ed53d8999b31437ee99e57e614d713ec379
JonathanStoll <jonafstoll@gmail.com>
2024-01-31 23:23:50 -0300

    button styling

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,11 +1,35 @@
-import {Text, View} from 'react-native'
+import { Text, View, Pressable, StyleSheet } from "react-native";
 
-function PrimaryButton ({children}){
-return <View>
-    <Text>
-        {children}
-    </Text>
-</View>
+function PrimaryButton({ children }) {
+  function pressHandeler() {
+    console.log("apretado");
+  }
+
+  return (
+    <View style={styles.buttonOuterContainer}>
+      <Pressable style={styles.buttonInerContainer} onPress={pressHandeler} android_ripple={{ color: "#340101" }}>
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
+    </View>
+  );
 }
 
-export default PrimaryButton
+export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    margin: 4,
+    borderRadius: 28,
+    overflow: "hidden",
+  },
+  buttonInerContainer: {
+    backgroundColor: "#4e0909",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+});
